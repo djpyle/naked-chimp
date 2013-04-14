a9f72f31ac5d690b9b6751aedea16df676407a06
Dustin Struckman <dustin@chimpchamp.com>
2013-04-14 15:09:05 -0700

    Formatting updates

--- index.php
+++ index.php
@@ -1,32 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-    <meta charset="utf-8">
-    <title>Subscribe to our newsletter</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="ChimpChamp.com">
+        <meta charset="utf-8">
+        <title>Subscribe to our newsletter</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="ChimpChamp.com">
     </head>
+<body>
 
-    <body>
-
-
-  <?php if(isset($_REQUEST['msg']) && $_REQUEST['msg'] == 'email_error'){?>
-      <p style="color:red;font-size:10px">The email entered is invalid</p>
-  <?php }?>
-  <?php if(isset($_REQUEST['msg']) && $_REQUEST['msg'] == 'success'){?>
-    <p style="color:green;font-size:10px">You have been subscribed successfully!</p>
-  <?php } ?>
-  <?php if(isset($_REQUEST['msg']) && $_REQUEST['msg'] == 'update'){?>
-    <p style="color:green;font-size:10px">You're already subscribed to this list.</p>
-  <?php } ?>
-      <!-- Main hero unit for a primary marketing message or call to action -->
+    <?php if(isset($_REQUEST['msg']) && $_REQUEST['msg'] == 'email_error'){?>
+        <p style="color:red;font-size:10px">The email entered is invalid</p>
+    <?php }?>
+    <?php if(isset($_REQUEST['msg']) && $_REQUEST['msg'] == 'success'){?>
+        <p style="color:green;font-size:10px">You have been subscribed successfully!</p>
+    <?php } ?>
+    <?php if(isset($_REQUEST['msg']) && $_REQUEST['msg'] == 'update'){?>
+        <p style="color:green;font-size:10px">You're already subscribed to this list.</p>
+    <?php } ?>
 
     <form action="mailchimp_action.php" method="post" class="">
-          <input type="hidden" value="" name="action">
-          First Name: <input type="text" value="" name="fname"><br>
-          Email: <input type="text" value="" name="email"><br>
-          <input type="submit" value="Submit" class="btn">
+        <input type="hidden" value="" name="action">
+        First Name: <input type="text" value="" name="fname"><br>
+        Email: <input type="text" value="" name="email"><br>
+        <input type="submit" value="Submit" class="btn">
     </form>
 
 </body>
