0392c0fa02d68b9df9fcb39720f601cb03861766
Dustin Struckman <dustin@chimpchamp.com>
2013-04-14 15:05:55 -0700

    Removed div classes

--- index.php
+++ index.php
@@ -11,15 +11,8 @@
     </head>
 
     <body>
-<header id="header" class="navbar navbar-fixed-top">
-      <div class="navbar navbar-inverse navbar-fixed-top">
-    <div class="navbar-inner">
-          <div class="container">
-      </div>
-        </div>
-  </div>
-    </header>
-<div class="container"> 
+
+
   <?php if(isset($_REQUEST['msg']) && $_REQUEST['msg'] == 'email_error'){?>
       <p style="color:red;font-size:10px">The email entered is invalid</p>
   <?php }?>
@@ -30,19 +23,14 @@
     <p style="color:green;font-size:10px">You're already subscribed to this list.</p>
   <?php } ?>
       <!-- Main hero unit for a primary marketing message or call to action -->
-      <div id="login_form">
+
     <form action="mailchimp_action.php" method="post" class="">
           <input type="hidden" value="" name="action">
           First Name: <input type="text" value="" name="fname"><br>
           Email: <input type="text" value="" name="email"><br>
           <input type="submit" value="Submit" class="btn">
-        </form>
-  </div>
-      <hr>
-      <footer>
-    <p>&copy; 2013</p>
-  </footer>
-    </div>
+    </form>
+
 <!-- /container --> 
 
 <!-- Le javascript
